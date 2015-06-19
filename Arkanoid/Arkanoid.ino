727f8f282412eccfcaaf82bb5d62ef3fde608999
Dipak <d.changur@live.nl>
2015-06-19 17:56:26 +0200

    tilt switch works

--- Arkanoid/Arkanoid.ino
+++ Arkanoid/Arkanoid.ino
@@ -9,13 +9,6 @@
 #define C   A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
-//TODO: Uitvinden waarom hij naar links beweegt (hoogstwaarschijnlijk staat poort 5 dan wel open). && waarom niet naar rechts met zelfde instellingen. Waar zit nog meer de fout dat het scherm maar 11 pixels is?!!
-//TODO: NOTE: Als hij nog wel naar rechts kan, geeft hij meteen Game Over, waarom?
-//TODO: NOTE: Als balx op 15 staat, doet hij het nog wel (als de rechter button ook ingeplugd is) anders meteen game over
-//TODO: NOTE: Als de code voor de balk naar de rechterkant naar boven staat, doet ie het wel naar rechts - maar niet meer naar links (zelfde fout). Ligt waarschijnlijk aan de .writeDisplay() code die nog gefixt moet worden.
-
-//TODO: Als de bar helemaal naar de zijkant is stopt hij bij 3 ipv bij 5 - dit moet nog gefixt worden. Waarschijnlijk bij if bar < 0, maar als je bar -2 doet, is de speler ook af.
-
 const uint8_t left = 5, right = 4; // Buttons
 uint8_t ballx = 5, bally = 14; // De locatie van de bal als het spel begint
 int speedx = -1, speedy = -1; // Ball's snelheid en beweging
@@ -42,9 +35,9 @@ uint8_t numBricks = 186; // count of bricks
 void setup() {
   matrix.begin();
   pinMode(right, INPUT);
-  digitalWrite(right, HIGH);
+
   pinMode(left, INPUT);
-  digitalWrite(left, HIGH);
+
 }
 
 
@@ -80,13 +73,13 @@ void paintBall() {
 // Checken op collisions met bovenkant, zijkanten en onderkant
 void checkCollisions() {
   if (numBricks > 0) { //als de bricks/steentjes meer dan 0 is, doe dit
-    
+
     // Onderkant
     if (bally == 14) {
-      
+
       // Kijken of de bar hier is, zo niet dan is speler game over
       if (ballx == bar || ballx == bar + 1 || ballx == bar + 2 || ballx == bar - 1 || ballx == bar - 2) {
-        speedy = -1; 
+        speedy = -1;
       }
       else {
         gameOver();
@@ -95,7 +88,6 @@ void checkCollisions() {
     // Bovenkant van het scherm
     if (bally == 0) {
       if (board[bally][ballx]) {
-        board[bally][ballx] = 0;
         numBricks--;
       }
       speedy = 1;
@@ -103,14 +95,13 @@ void checkCollisions() {
     // Zijkanten van het scherm
     if ( ballx == 0 || ballx == 31) {
       if (board[bally][ballx]) {
-        board[bally][ballx] = 0;
         numBricks--;
         speedy = 1;
       }
       speedx = -speedx;
     }
     // Als bal het board raakt, gaan de bricks weg
-    if (ballx != 0 && ballx != 32 && bally != 0 && bally != 31) {
+    if (ballx != 0 && ballx != 32 && bally != 0 && bally != 32) {
       if (board[bally][ballx]) {
         board[bally][ballx] = 0;
         numBricks--;
@@ -123,26 +114,52 @@ void checkCollisions() {
   else win(); //anders (als alle bricks/steentjes op zijn) gewonnen
 }
 
+int buttonState = 0;
+int buttonState2 = 0;
+
 void loop() {
   matrix.fillScreen(matrix.Color333(0, 0, 0)); //vul het scherm naar zwart om het scherm te hertekenen
-  //matrix.clear();
   paintBricks();
   paintBar();
   paintBall();
 
+  buttonState = digitalRead(right);
+  buttonState2 = digitalRead(left);
 
-  //matrix.writeDisplay(); //TODO: Kijken hoe dit gefixt kan worden, hij pakt deze command niet
-  if (!digitalRead(left)) {
-    if (bar >= 3) {
-      bar--;
+  if (buttonState == HIGH) {
+    digitalWrite(right, HIGH);
+
+    if (digitalRead(right)) {
+      if (bar <= 28) {
+        bar = bar + 2;
+      }
+
+    }
+  } else {
+
+    if (buttonState2 == HIGH) {
+      digitalWrite(right, LOW);
+      digitalWrite(left, HIGH);
+
+      if (digitalRead(left)) {
+        if (bar >= 3) {
+          bar = bar - 2;
+        }
+
+      }
     }
   }
-  if (!digitalRead(right)) {
-    if (bar <= 28) {
-      bar++;
-    }
-
-  }
+  //  if (!digitalRead(left)) {
+  //    if (bar >= 3) {
+  //      bar--;
+  //    }
+  //  }
+  //  if (!digitalRead(right)) {
+  //    if (bar <= 28) {
+  //      bar++;
+  //    }
+  //
+  //  }
   checkCollisions();
   delay(200);
 }
