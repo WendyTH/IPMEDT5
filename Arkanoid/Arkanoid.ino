d4fede9add697f4f06c9b492dfd852037f61f9a0
edoglaz <edoglaz@yahoo.com>
2015-06-30 13:19:29 +0200

    kleine aanpassingen gefixt

--- Arkanoid/Arkanoid.ino
+++ Arkanoid/Arkanoid.ino
@@ -434,59 +434,13 @@ int tempo[] = {
 int introSound[] = {
 NOTE_C8, NOTE_CS8, NOTE_D8, NOTE_DS8
   
-//  NOTE_G7, NOTE_G7, 0, NOTE_C8,
-//  0, NOTE_C7, NOTE_E7, 0,
-//  NOTE_G7, 0, 0,  0,
-//  NOTE_G6, 0, 0, 0,
-//
-//  NOTE_C7, 0, 0, NOTE_G6,
-//  0, 0, NOTE_E6, 0,
-//  0, NOTE_A6, 0, NOTE_B6,
-//  0, NOTE_AS6, NOTE_A6, 0,
-//
-//  NOTE_G6, NOTE_E7, NOTE_G7,
-//  NOTE_A7, 0, NOTE_F7, NOTE_G7,
-//  0, NOTE_E7, 0, NOTE_C7,
-//  NOTE_D7, NOTE_B6, 0, 0,
-//
-//  NOTE_C7, 0, 0, NOTE_G6,
-//  0, 0, NOTE_E6, 0,
-//  0, NOTE_A6, 0, NOTE_B6,
-//  0, NOTE_AS6, NOTE_A6, 0,
-//
-//  NOTE_G6, NOTE_E7, NOTE_G7,
-//  NOTE_A7, 0, NOTE_F7, NOTE_G7,
-//  0, NOTE_E7, 0, NOTE_C7,
-//  NOTE_D7, NOTE_B6, 0, 0
+
 };
 
 int tempoIntro[] = {
 
   3,10,10,10
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//
-//  9, 9, 9,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//
-//  9, 9, 9,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
-//  12, 12, 12, 12,
+
 };
 
 
@@ -498,19 +452,16 @@ void sing(int s) {
     int size = sizeof(melodybrick) / sizeof(int);
     for (int thisNote = 0; thisNote < size; thisNote++) {
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+     
       int noteDuration = 1000 / tempobrick[thisNote];
 
       buzz(melodyPin, melodybrick[thisNote], noteDuration);
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
+    
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
 
-      // stop the tone playing:
+      
       buzz(melodyPin, 0, noteDuration);
 
     }
@@ -521,24 +472,21 @@ void sing(int s) {
     int size = sizeof(melodybar) / sizeof(int);
     for (int thisNote = 0; thisNote < size; thisNote++) {
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      
       int noteDuration = 1000 / tempobar[thisNote];
 
       buzz(melodyPin, melodybar[thisNote], noteDuration);
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
+     
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
 
-      // stop the tone playing:
+      
       buzz(melodyPin, 0, noteDuration);
 
     }
   }
-  // iterate over the notes of the melody:
+
   song = s;
 
 
@@ -548,19 +496,16 @@ void sing(int s) {
     int size = sizeof(melody) / sizeof(int);
     for (int thisNote = 0; thisNote < size; thisNote++) {
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      
       int noteDuration = 800 / tempo[thisNote];
 
       buzz(melodyPin, melody[thisNote], noteDuration);
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
+      
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
 
-      // stop the tone playing:
+      
       buzz(melodyPin, 0, noteDuration);
 
     }
@@ -572,19 +517,16 @@ void sing(int s) {
     int size = sizeof(introSound) / sizeof(int);
     for (int thisNote = 0; thisNote < size; thisNote++) {
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      
       int noteDuration = 800 / tempoIntro[thisNote];
 
       buzz(melodyPin, introSound[thisNote], noteDuration);
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
+      
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
 
-      // stop the tone playing:
+     
       buzz(melodyPin, 0, noteDuration);
 
     }
@@ -592,17 +534,15 @@ void sing(int s) {
 }
 void buzz(int targetPin, long frequency, long length) {
   digitalWrite(13, HIGH);
-  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
-  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
-  //// there are two phases to each cycle
-  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
-  //// multiply frequency, which is really cycles per second, by the number of seconds to
-  //// get the total number of cycles to produce
-  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
-    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
-    delayMicroseconds(delayValue); // wait for the calculated delay value
-    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
-    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  long delayValue = 1000000 / frequency / 2; 
+  
+  long numCycles = frequency * length / 1000; 
+  
+  for (long i = 0; i < numCycles; i++) { 
+    digitalWrite(targetPin, HIGH); 
+    delayMicroseconds(delayValue); 
+    digitalWrite(targetPin, LOW); 
+    delayMicroseconds(delayValue); 
   }
   digitalWrite(13, LOW);
 }
